56e2d673cf361ae19bc5cbaf785292bdec548046
ThomasAriza1227 <arizathomas2007@gmail.com>
2026-09-08 09:54:07 -0500

    Se agregaron algunas funciones y se remplazaron algunos codigos

--- src/services/equipoApi.js
+++ src/services/equipoApi.js
@@ -1,11 +1,15 @@
-const API = "http://localhost:3001/equipo";
-
-export const obtenerEquipo = async () => {
-    const respuesta = await fetch(API);
+export const agregarAlEquipo = async (pokemon) => {
+    const respuesta = await fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pokemon)
+    });
 
     if (!respuesta.ok) {
         throw new Error(
-            "No fue posible cargar el equipo"
+            "No fue posible agregar el Pokémon"
         );
     }
 
